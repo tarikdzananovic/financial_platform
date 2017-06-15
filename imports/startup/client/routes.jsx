f6d029f3da86428a17538d288bc1aa7e1011902b
Tarik Dzananovic <tarik.dzananovic@softhouse.se>
2017-06-15 14:54:00 +0200

    Add forget and reset password routines

--- imports/startup/client/routes.jsx
+++ imports/startup/client/routes.jsx
@@ -7,7 +7,18 @@ import App from '../../ui/layouts/App.jsx';
 import Index from '../../ui/layouts/Index.jsx';
 import Signup from '../../ui/pages/Signup.jsx';
 import Login from '../../ui/pages/Login.jsx';
+import RecoverPassword from '../../ui/pages/RecoverPassword.jsx';
+import ResetPassword from '../../ui/pages/ResetPassword.jsx';
 import NotFound from '../../ui/pages/NotFound.jsx';
+
+const authenticate = (nextState, replace) => {
+    if (!Meteor.loggingIn() && !Meteor.userId()) {
+        replace({
+            pathname: '/login',
+            state: { nextPathname: nextState.location.pathname },
+        });
+    }
+};
 
 
 Meteor.startup(() => {
@@ -17,6 +28,8 @@ Meteor.startup(() => {
                 <IndexRoute name="index" component={ Index } />
                 <Route name="signup" path="/signup" component={ Signup } />
                 <Route name="login" path="/login" component={ Login } />
+                <Route name="recover-password" path="/recover-password" component={ RecoverPassword } />
+                <Route name="reset-password" path="/reset-password/:token" component={ ResetPassword }/>
                 <Route path="*" component={ NotFound } />
             </Route>
         </Router>,
