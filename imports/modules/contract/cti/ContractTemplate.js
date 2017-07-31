c67f61203e7c05227637d8c9bc6d374544e7ae59
Tarik Dz <tareeus@gmail.com>
2017-07-31 17:41:33 +0200

    Start agent service types

--- imports/modules/contract/cti/ContractTemplate.js
+++ imports/modules/contract/cti/ContractTemplate.js
@@ -13,6 +13,37 @@ var getObjectType = (type, inputName, parameter) => {
     object.value = undefined;
     object.parameter = parameter;
     return object;
+};
+
+/*var addMapSubObject  = (object, key, value) => {
+    object[key] = value;
+    return object;
+};*/
+
+var getMapObjectType = (type, inputName) => {
+    var object = {};
+    object.type= type;
+    object.inputName = inputName;
+
+    //TODO:: get Values from API
+    var value = [];
+
+    var value1 = {};
+    var value2= {};
+    value1.name = "youtube_b_lclz";
+    value1.shortDescription = "YouTube localized banners";
+    value1.description= "ad campaing using YouTube video banners locallized for you biz service area";
+    value1.docUrl = "";
+
+    value2.name = "fb_lclz_banners";
+    value2.shortDescription = "FaceBook localized ad campaing";
+    value2.description = "ad campaing runnning on FaceBook tuned for you biz service area";
+    value2.docUrl = "";
+
+    value.push(value1);
+    value.push(value2);
+    object.value = value;
+    return object;
 }
 
 var getAdvertisingTemplate = () =>
@@ -26,7 +57,7 @@ var getAdvertisingTemplate = () =>
     legalIds["Company Legal ID"] = getObjectType("text", "companyLegalId", "{CompanyLegalID}");
     legalIds["Agent Legal ID"] = getObjectType("text", "agentLegalId", "{AgentLegalID}");
     var contractTerms = {};
-    contractTerms["Agent Service Type"] = getObjectType("text", "agentServiceType", "{AgentServiceType}");
+    contractTerms["Agent Service Types"] = getMapObjectType("grid_check", "agentServiceTypes");
     contractTerms["Start Date"] = getObjectType("date", "startDate", "{StartDate}");
     contractTerms["End Date"] = getObjectType("date", "endDate", "{EndDate}");
     contractTerms["Compensation Amount"] = getObjectType("number", "amount", "{CompensationAmount}");
